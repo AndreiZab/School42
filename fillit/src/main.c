fa890f1977677068fe7f763336dd760236e8dddc
Rena Healitt <rhealitt@oa-h1.21-school.ru>
2019-04-23 19:28:48 +0300

    Tue 19:28

--- fillit/src/main.c
+++ fillit/src/main.c
@@ -1,4 +1,4 @@
-/* ************************************************************************** */
+/* ************************************************************************* */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -6,7 +6,7 @@
 /*   By: rhealitt <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/04/19 17:25:28 by rhealitt          #+#    #+#             */
-/*   Updated: 2019/04/23 18:52:36 by rhealitt         ###   ########.fr       */
+/*   Updated: 2019/04/23 19:25:54 by rhealitt         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,17 +27,11 @@ int		main(int argc, char **argv)
 	if (argc != 2)
 		return (message(0, "usage: fillit source_file\n"));
 	fd = open(argv[1], O_RDONLY);
+ 	if (fd == -1)
+		return (message(1, "error\n"));
 	ret = ft_read(fd, buf);
 	if (ret < 0)
-	{
 		return (message(1, "error\n"));
-	}
-	if (fd == -1 || !size_validation(buf) || !connect_validation(buf))
-	{
-		printf("%d", size_validation(buf));
-		printf("%d", connect_validation(buf));
-		return (message(1, "error\n"));
-	}
 	close(fd);
 	return (0);
 }
