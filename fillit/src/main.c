36b16c8069c6161e17fec672ead7cb79412ed4bb
Leyla Arlyne <larlyne@e6r4p8.21-school.ru>
2019-04-27 10:43:11 +0300

    main.c The Norm

--- fillit/src/main.c
+++ fillit/src/main.c
@@ -1,4 +1,4 @@
-/* ************************************************************************* */
+/* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
@@ -20,17 +20,17 @@ int		message(int ret, char *str)
 
 int		main(int argc, char **argv)
 {
-	int		fd;
-	ssize_t	ret;
-	t_termino **terms;
-	int count;
+	int			fd;
+	ssize_t		ret;
+	t_termino	**terms;
+	int			count;
 
 	terms = (t_termino**)malloc(sizeof(t_termino*) * 26);
 	count = 0;
 	if (argc != 2)
 		return (message(0, "usage: fillit source_file\n"));
 	fd = open(argv[1], O_RDONLY);
- 	if (fd == -1)
+	if (fd == -1)
 		return (message(1, "error\n"));
 	ret = ft_read(fd, &terms, &count);
 	if (ret < 0 || (ret < 20 && ret > 0))
