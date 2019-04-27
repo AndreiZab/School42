c5dbfc4d128dd0fe6d24bd43568d74655e925148
Leyla Arlyne <larlyne@e6r4p8.21-school.ru>
2019-04-27 10:41:38 +0300

    parcer.c The Norm

--- fillit/src/parser.c
+++ fillit/src/parser.c
@@ -12,73 +12,47 @@
 
 #include "fillit.h"
 
-char        **get_arr(char *s, char ox, char oy, char w, char  h)
+char		**get_arr(char *s, char ox, char oy, t_termino *elem)
 {
-    char    **arr;
-    int     y;
+	char	**arr;
+	int		y;
 
-    arr = (char**)malloc(sizeof(char*) * h );
-    y = 0;
-    while (y < h)
-    {
-        arr[y] = (char*)malloc(w);
-        ft_memcpy(arr[y], s + (y + oy) * 5 + ox, w);
-        ++y;
-    }
-    return (arr);
+	arr = (char**)malloc(sizeof(char*) * elem->height);
+	y = 0;
+	while (y < elem->height)
+	{
+		arr[y] = (char*)malloc(elem->width);
+		ft_memcpy(arr[y], s + (y + oy) * 5 + ox, elem->width);
+		++y;
+	}
+	return (arr);
 }
 
-t_termino   *parse_termino(char *str)
+t_termino	*parse_termino(char *str)
 {
-    t_termino   *elem;
-    char *pos; //0 - xmin; 1 - ymin; 2 - xmax; 3 - ymax
-    int i;
+	t_termino	*elem;
+	char		*pos;
+	int			i;
 
-    elem = (t_termino*)malloc(sizeof(t_termino));
-    pos = ft_strnew(3);
-    pos[0] = 5;
-    pos[1] = 5;
-    i = 0;
-    while (str[i])
-    {
-        if (str[i] == '#')
-        {
-            if (i % 5 < pos[0])
-                pos[0] = i % 5;
-            if (i % 5 > pos[2])
-                pos[2] = i % 5;
-            if (i / 5 < pos[1])
-                pos[1] = i / 5;
-            if (i / 5 > pos[3])
-                pos[3] = i / 5;
-        }
-        ++i;
-    }
-    elem->width = pos[2] - pos[0] + 1;
-    elem->height = pos[3] - pos[1] + 1;
-    elem->arr = get_arr(str, pos[0], pos[1], elem->width, elem->height);
-    return (elem);
-}
-
-#include <stdio.h>
-
-void    show_termino(t_termino *elem)
-{
-    int y;
-    int x;
-
-    y = 0;
-    printf("W: %d, H: %d\n", (int)elem->width, (int)elem->height);
-    while (y < elem->height)
-    {
-        x = 0;
-        while (x < elem->width)
-        {
-            ft_putchar(elem->arr[y][x]);
-            ++x;
-        }
-        ft_putchar('\n');
-        ++y;
-    }
-    ft_putchar('\n');
+	elem = (t_termino*)malloc(sizeof(t_termino));
+	pos = ft_strnew(3);
+	pos[0] = 5;
+	pos[1] = 5;
+	i = -1;
+	while (str[++i])
+		if (str[i] == '#')
+		{
+			if (i % 5 < pos[0])
+				pos[0] = i % 5;
+			if (i % 5 > pos[2])
+				pos[2] = i % 5;
+			if (i / 5 < pos[1])
+				pos[1] = i / 5;
+			if (i / 5 > pos[3])
+				pos[3] = i / 5;
+		}
+	elem->width = pos[2] - pos[0] + 1;
+	elem->height = pos[3] - pos[1] + 1;
+	elem->arr = get_arr(str, pos[0], pos[1], elem);
+	return (elem);
 }
